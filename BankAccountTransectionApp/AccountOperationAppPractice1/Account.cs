43dd9c48a9e01a9a416a8167f94368968d1af969
sohelrkhan <55046835+sohelrkhan@users.noreply.github.com>
2019-10-18 15:38:25 +0600

    Bank App

---
+++ BankAccountTransectionApp/AccountOperationAppPractice1/Account.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccountOperationAppPractice1
+{
+    class Account
+    {
+        public int acnumber;
+        public string cname;
+        private double balance;
+
+        public double Deposit(double amount)
+        {
+            balance = balance + amount;
+            return balance ;
+
+        }
+        public double withdraw(double amount)
+        {
+            balance= balance - amount;
+            return balance;
+        }
+        public string getinfo()
+        {
+            var msg = cname + " Your account number:" + acnumber + "and its balance:" + balance;
+            return msg;
+        }
+    }
+}
---
+++ BankAccountTransectionApp/AccountOperationAppPractice1/AccountOperationUi.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AccountOperationAppPractice1
+{
+    public partial class AccountOperationUi : Form
+    {
+        Account _account = new Account();
+        public AccountOperationUi()
+        {
+            InitializeComponent();
+        }
+       
+
+        private void CreateButton_Click(object sender, EventArgs e)
+        {
+            
+            _account.acnumber = Convert.ToInt32(accountNoTextBox.Text);
+            _account.cname = customerNameTextBox.Text;
+
+
+        }
+
+        private void DipositButton_Click(object sender, EventArgs e)
+        {
+           
+            double amount = Convert.ToDouble(amountTextBox.Text);
+           double deposit= _account.Deposit(amount);
+
+        }
+
+        private void WithdrawButton_Click(object sender, EventArgs e)
+        {
+            
+            double amount = Convert.ToDouble(amountTextBox.Text);
+            double withdraw=_account.withdraw(amount);
+
+        }
+
+        private void ReportButton_Click(object sender, EventArgs e)
+        {
+            string message =_account.getinfo();
+            MessageBox.Show(message);
+        }
+    }
+}
